555a7d0e165155c49a8e00b8714a72ceda0ab26b
Dmitry Tarasov <dimatarasov825@gmail.com>
2025-10-07 17:47:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,18 +36,18 @@ SUBSCRIPTION_PLANS: Dict[str, Dict] = {
     "1_month": {
         "name": "1 месяц",
         "days": 30,
-        "price": 99,
+        "price": 149,
     },
     "3_months": {
         "name": "3 месяца",
         "days": 90,
-        "price": 249,
+        "price": 299,
         
     },
     "6_months": {
         "name": "6 месяцев",
         "days": 180,
-        "price": 399,
+        "price": 449,
         
     }
 }
